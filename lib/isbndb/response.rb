82b71d050e5e8a831edecf6583330b1c72f47e3e
Lazlo Sitzer <lazlo@lazlo.de>
2009-08-06 20:04:42 +0200

    renamed Response.load_from_file to load_file.

--- lib/isbndb/response.rb
+++ lib/isbndb/response.rb
@@ -3,7 +3,7 @@ require 'rexml/document'
 module ISBNdb
   class BaseResponse
     attr_reader :content
-    def load_from_file(file)
+    def load_file(file)
       @content = REXML::Document.new(File.read(file))
     end
   end
--- test/test_base_response.rb
+++ test/test_base_response.rb
@@ -6,10 +6,10 @@ class TestBaseResponse < Test::Unit::TestCase
     @res = ISBNdb::BaseResponse.new
   end
 
-  def test_load_from_file
+  def test_load_file
     file = 'test/fixtures/query-for-tdd-books-result.xml'
     assert_nil( @res.content )
-    assert( @res.load_from_file(file) )
+    assert( @res.load_file(file) )
     assert_not_nil( @res.content )
   end
 end
