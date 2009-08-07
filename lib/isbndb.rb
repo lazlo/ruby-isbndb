76bf416db4e220c7aa8a536e83e7a8f0ef5027c8
Lazlo Sitzer <lazlo@lazlo.de>
2009-08-07 05:13:56 +0200

    updated isbndb.rb with require statements to new files.

--- lib/isbndb.rb
+++ lib/isbndb.rb
@@ -3,3 +3,9 @@ end
 
 require 'isbndb/request.rb'
 require 'isbndb/response.rb'
+require 'isbndb/find.rb'
+require 'isbndb/book.rb'
+require 'isbndb/subject.rb'
+require 'isbndb/category.rb'
+require 'isbndb/author.rb'
+require 'isbndb/publisher.rb'
